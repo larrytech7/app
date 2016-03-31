a4fd67100169b412a88924a63a23a0d6cd8017f9
Larry_Lite <larryakah@gmail.com>
2016-03-31 16:59:14 +0100

    databse config

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'icepay',
 			'username'  => 'root',
-			'password'  => 'toor',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
